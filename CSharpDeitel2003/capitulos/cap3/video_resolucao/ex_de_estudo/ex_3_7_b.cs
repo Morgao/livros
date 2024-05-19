fa5aae8f06cc5aa0f9a156ba456c862a087ae2f4
Morgao <contatomorgao@gmail.com>
2024-05-19 13:08:40 -0300

    Create ex_3_7_b.cs

---
+++ CSharpDeitel2003/capitulos/cap3/video_resolucao/ex_de_estudo/ex_3_7_b.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // de 1 a 40 na mesma linha
+
+        Console.Write("12 34 56 78 910");
+        Console.Write("1112 1314 1516 1718 1920"); 
+        Console.Write("2122 2324 2526 2728 2930 "); 
+        Console.Write("3132 3334 3536 3738 3940");
+
+
+    }
+}
