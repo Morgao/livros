ece9705af349f4ac4d4f71eae602a1ea60463ae2
Morgao <contatomorgao@gmail.com>
2024-05-19 13:09:34 -0300

    Create ex_3_10.cs

---
+++ CSharpDeitel2003/capitulos/cap3/video_resolucao/ex_de_estudo/ex_3_10.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Net.NetworkInformation;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ 
+        Console.WriteLine("*******         ***         *         *                ");
+        Console.WriteLine("*     *        *   *       ***      *   *              ");
+        Console.WriteLine("*     *       *     *     *****    *     *             ");
+        Console.WriteLine("*     *       *     *       *     *       *            ");
+        Console.WriteLine("*     *       *     *       *    *         *           ");
+        Console.WriteLine("*     *       *     *       *     *       *            ");
+        Console.WriteLine("*     *       *     *       *      *     *             ");
+        Console.WriteLine("*     *        *   *        *       *   *              ");
+        Console.WriteLine("*******         ***         *         *                ");
+       
+
+
+     
+
+      
+
+
+    }
+}
