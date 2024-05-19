a4a3ba6dfe306630a19b7b77acd35ed7ae932b5e
Morgao <contatomorgao@gmail.com>
2024-05-19 13:05:04 -0300

    Create 06.cs

---
+++ CSharpDeitel2003/capitulos/cap3/video_resolucao/ex_do_cap/06.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Diagnostics.Contracts;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+       int x = 10;
+
+     Console.WriteLine(5 + 2);
+     Console.WriteLine(x + 2);
+     Console.WriteLine("x + {0}",x);
+
+        
+
+    }
+}
