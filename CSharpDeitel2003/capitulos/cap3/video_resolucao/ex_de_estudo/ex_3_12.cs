5c3983cbe158566a355f51838a5778c9abfde58c
Morgao <contatomorgao@gmail.com>
2024-05-19 13:10:29 -0300

    Create ex_3_12.cs

---
+++ CSharpDeitel2003/capitulos/cap3/video_resolucao/ex_de_estudo/ex_3_12.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Net.NetworkInformation;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ 
+        Console.WriteLine("*\n**\n***\n****\n*****");
+       
+
+
+     
+
+      
+
+
+    }
+}
