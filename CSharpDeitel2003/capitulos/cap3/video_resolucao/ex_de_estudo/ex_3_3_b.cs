6016ad65a06a64743b747dd931072fde56c9826e
Morgao <contatomorgao@gmail.com>
2024-05-19 13:06:20 -0300

    Create ex_3_3_b.cs

---
+++ CSharpDeitel2003/capitulos/cap3/video_resolucao/ex_de_estudo/ex_3_3_b.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Diagnostics.Contracts;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        int a, b,c;
+        b =2;
+        c = 4;
+        a = b*c;
+        Console.WriteLine("valor do resultado {0}", a);
+        
+
+    }
+}
