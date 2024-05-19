3c32d1ab4e0fbf7a0debabcc02b87fab385fe0ea
Morgao <contatomorgao@gmail.com>
2024-05-19 13:11:27 -0300

    Create ex_3_15.cs

---
+++ CSharpDeitel2003/capitulos/cap3/video_resolucao/ex_de_estudo/ex_3_15.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Net.NetworkInformation;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // A B C a b c 0 1 2 $ * + /
+        Console.WriteLine("o caracter " + 'A' + " tem valor " + (int)'A');
+        Console.WriteLine("o caracter " + 'B' + " tem valor " + (int)'B');
+        Console.WriteLine("o caracter " + 'C' + " tem valor " + (int)'C');
+        Console.WriteLine("o caracter " + 'a' + " tem valor " + (int)'a');
+        Console.WriteLine("o caracter " + 'b' + " tem valor " + (int)'b');
+        Console.WriteLine("o caracter " + 'c' + " tem valor " + (int)'c');
+        Console.WriteLine("o caracter " + '0' + " tem valor " + (int)'0');
+        Console.WriteLine("o caracter " + '1' + " tem valor " + (int)'1');
+        Console.WriteLine("o caracter " + '2' + " tem valor " + (int)'2');
+        Console.WriteLine("o caracter " + '$' + " tem valor " + (int)'$');
+        Console.WriteLine("o caracter " + '*' + " tem valor " + (int)'*');
+        Console.WriteLine("o caracter " + '+' + " tem valor " + (int)'+');
+        Console.WriteLine("o caracter " + '/' + " tem valor " + (int)'/');
+
+
+
+        /*
+        o caracter A tem valor 65
+        o caracter B tem valor 66
+        o caracter C tem valor 67
+        o caracter a tem valor 97
+        o caracter b tem valor 98
+        o caracter c tem valor 99
+        o caracter 0 tem valor 48
+        o caracter 1 tem valor 49
+        o caracter 2 tem valor 50
+        o caracter $ tem valor 36
+        o caracter * tem valor 42
+        o caracter + tem valor 43
+        o caracter / tem valor 47
+        */
+
+
+
+
+
+
+    }
+}
