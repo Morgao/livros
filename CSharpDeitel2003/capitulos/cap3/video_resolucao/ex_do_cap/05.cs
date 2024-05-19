3f648008a062d8f751c79609159e01168c8e7ee6
Morgao <contatomorgao@gmail.com>
2024-05-19 13:04:49 -0300

    Create 05.cs

---
+++ CSharpDeitel2003/capitulos/cap3/video_resolucao/ex_do_cap/05.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Diagnostics.Contracts;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            // programa para somar 2 numeros inteiros
+        string  segundoNumero;
+
+        int primeiro, segundo, soma;
+
+
+
+        
+
+     
+
+        Console.WriteLine("Digite o primeiro Número: ");
+        primeiro = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo Número: ");
+        segundoNumero = Console.ReadLine();
+
+
+        
+        segundo = Int32.Parse((segundoNumero));
+
+        soma = primeiro + segundo;
+
+        Console.WriteLine("\n A soma num1 = {0} ", sum);
+
+    }
+}
