ba55a156b9cfaab3dac923c8809f583c2b85caf7
Morgao <contatomorgao@gmail.com>
2024-05-19 13:10:13 -0300

    Create ex_3_11.cs

---
+++ CSharpDeitel2003/capitulos/cap3/video_resolucao/ex_de_estudo/ex_3_11.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace capitulo02_cs_deitel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x, y;
+
+            x = 2;
+            y = 3;
+
+
+
+
+
+            // MessageBox.Show("x =");
+
+            MessageBox.Show(           "*******         ***         *         *                \n*     *        *   *       ***      *   *              \n*     *       *     *     *****    *     *             \n*     *       *     *       *     *       *            \n*     *       *     *       *    *         *           \n*     *       *     *       *     *       *            \n*     *       *     *       *      *     *             \n*     *        *   *        *       *   *              \n*******         ***         *         *                \n");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
