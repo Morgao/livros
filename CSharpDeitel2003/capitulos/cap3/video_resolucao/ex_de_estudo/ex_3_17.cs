67cf96473194653f80ac365624fee06229f361e2
Morgao <contatomorgao@gmail.com>
2024-05-19 13:12:07 -0300

    Create ex_3_17.cs

---
+++ CSharpDeitel2003/capitulos/cap3/video_resolucao/ex_de_estudo/ex_3_17.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Formats.Asn1;
+using System.Net.NetworkInformation;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ 
+      // numero 0 10
+      // numero quadradro cubo
+      int a0,a1,a2,a3,a4,a5,a6,a7,a8,a9,a10;
+      a0 = 0;
+      a1 = 1;
+      a2 = 2;
+      a3 = 3;
+      a4 = 4;
+      a5 = 5;
+      a6 = 6;
+      a7 = 7;
+      a8 = 8;
+      a9 = 9;
+      a10 = 10;
+
+      Console.WriteLine("Numero        Quadrado        Cubo");
+      Console.WriteLine(+a0+"                "+a0*a0+"              "+a0*a0*a0);
+      Console.WriteLine(+a1+"                "+a1*a1+"              "+a1*a1*a1);
+      Console.WriteLine(+a2+"                "+a2*a2+"              "+a2*a2*a2);
+      Console.WriteLine(+a3+"                "+a3*a3+"              "+a3*a3*a3);
+      Console.WriteLine(+a4+"                "+a4*a4+"             "+a4*a4*a4);
+      Console.WriteLine(+a5+"                "+a5*a5+"             "+a5*a5*a5);
+      Console.WriteLine(+a6+"                "+a6*a6+"             "+a6*a6*a6);
+      Console.WriteLine(+a7+"                "+a7*a7+"             "+a7*a7*a7);
+      Console.WriteLine(+a8+"                "+a8*a8+"             "+a8*a8*a8);
+      Console.WriteLine(+a9+"                "+a9*a9+"             "+a9*a9*a9);
+      Console.WriteLine(+a10+"               "+a10*a10+"            "+a10*a10*a10);
+     
+    }
+}
