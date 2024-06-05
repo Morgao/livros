5fe554cb27301c220fcdc91351699c8f7520c1a6
Morgao <contatomorgao@gmail.com>
2024-06-04 23:50:54 -0300

    Create ex_4_5_e.cs

---
+++ CSharpDeitel2003/capitulos/cap4/video_resolucao/ex_de_estudo/ex_4_5_e.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Formats.Asn1;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ 
+   
+   int  soma,x;
+   x = 1;
+   soma  = 0;
+   soma = soma + x;
+   Console.WriteLine("A soma é: {0}", soma);
+      
+
+
+
+
+  
+     
+    } // fim da main
+
+} // fim do programa
