f763065d9e07daee14b5142ab04f18989f0ec3b8
Morgao <contatomorgao@gmail.com>
2024-06-04 23:49:36 -0300

    Create ex_4_5_a.cs

---
+++ CSharpDeitel2003/capitulos/cap4/video_resolucao/ex_de_estudo/ex_4_5_a.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Formats.Asn1;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ 
+   
+   int  soma,x;
+
+      
+
+
+
+
+  
+     
+    } // fim da main
+
+} // fim do programa
