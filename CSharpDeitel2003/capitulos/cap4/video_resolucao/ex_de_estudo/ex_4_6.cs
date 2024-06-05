1ccc332e975f98594279fa910c56c73214228b77
Morgao <contatomorgao@gmail.com>
2024-06-04 23:51:23 -0300

    Create ex_4_6.cs

---
+++ CSharpDeitel2003/capitulos/cap4/video_resolucao/ex_de_estudo/ex_4_6.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Formats.Asn1;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ 
+   
+   int  x, y, soma;
+   x = 1;
+   y = 1;
+
+   while(x <= 10){
+      soma = x + y;
+      
+      Console.WriteLine(soma);
+      x++;
+      y++;
+   }
+      
+
+
+
+
+  
+     
+    } // fim da main
+
+} // fim do programa
