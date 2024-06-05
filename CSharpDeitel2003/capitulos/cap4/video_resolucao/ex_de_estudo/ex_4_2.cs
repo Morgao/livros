45aeca2905863e0903800337e9f4214b4cde82f3
Morgao <contatomorgao@gmail.com>
2024-06-04 23:41:06 -0300

    Create ex_4_2.cs

---
+++ CSharpDeitel2003/capitulos/cap4/video_resolucao/ex_de_estudo/ex_4_2.cs
@@ -0,0 +1,37 @@
+/*
+a) Falsa, o pseudocódigo deve ser facilmente convertido em código C#.
+b) Verdadeira.
+c) Falsa, Alguns programadores dizem que as intruções goto violam a programação estruturada e causam problemas consideusing System;
+namespace teste2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+    /*
+a) Falsa, o pseudocódigo deve ser facilmente convertido em código C#.
+b) Verdadeira.
+c) Falsa, Alguns programadores dizem que as intruções goto violam a programação estruturada e causam problemas consideráveis.
+d) Verdadeira.
+e) verdadeira.
+f) Falsa, A estrutura de sequência está incorporada ao C#; as linhas de código são executadas na ordem em que são escritas, a não ser que seja explicitamente intruído de outra forma.
+g) Verdadeira.
+h) Falsa, colocar um  ponto e vírgula após a condição em uma estrutura if normalmente é um erro lógico.
+i) Verdadeira.
+*/
+
+
+
+
+        }
+            
+    }
+}áveis.
+d) Verdadeira.
+e) verdadeira.
+f) Falsa, A estrutura de sequência está incorporada ao C#; as linhas de código são executadas na ordem em que são escritas, a não ser que seja explicitamente intruído de outra forma.
+g) Verdadeira.
+h) Falsa, colocar um  ponto e vírgula após a condição em uma estrutura if normalmente é um erro lógico.
+i) Verdadeira.
+*/
