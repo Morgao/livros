d2d0e7e879f8cce02698115ece1277c7ea6137f4
Morgao <contatomorgao@gmail.com>
2024-06-04 22:09:00 -0300

    Create 07.cs

---
+++ CSharpDeitel2003/capitulos/cap4/video_resolucao/ex_do_cap/07.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Formats.Asn1;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ 
+     //  pre incremento e pos incremento diferenca
+     int c;
+
+     c = 5;
+     Console.WriteLine(c);
+     Console.WriteLine(c++); //  pos incremento
+     Console.WriteLine(c);
+
+     Console.WriteLine();
+     
+     c = 5;
+     Console.WriteLine(c);
+     Console.WriteLine(++c); // pre incremento
+     Console.WriteLine(c);
+
+      
+
+
+
+
+  
+     
+    } // fim da main
+
+} // fim do programa
