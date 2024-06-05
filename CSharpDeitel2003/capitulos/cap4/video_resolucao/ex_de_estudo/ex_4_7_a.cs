bedc9b5f588989956093ac53607450d79b012853
Morgao <contatomorgao@gmail.com>
2024-06-04 23:52:03 -0300

    Create ex_4_7_a.cs

---
+++ CSharpDeitel2003/capitulos/cap4/video_resolucao/ex_de_estudo/ex_4_7_a.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Formats.Asn1;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ 
+   
+   int produto = 5, x = 5;
+
+   produto *= x++;
+   Console.WriteLine(produto);
+      
+
+
+
+
+  
+     
+    } // fim da main
+
+} // fim do programa
