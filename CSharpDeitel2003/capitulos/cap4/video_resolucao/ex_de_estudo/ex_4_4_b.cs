37ac66a2b5177e9383220dd184f683a68f788273
Morgao <contatomorgao@gmail.com>
2024-06-04 23:42:19 -0300

    Create ex_4_4_b.cs

---
+++ CSharpDeitel2003/capitulos/cap4/video_resolucao/ex_de_estudo/ex_4_4_b.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Formats.Asn1;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ 
+   
+     int cont = 15;
+
+     if (cont > 10)
+     {
+      Console.WriteLine("a variavel cont {0} é maior do que 10", cont);
+     }
+         
+     
+      
+
+
+
+
+  
+     
+    } // fim da main
+
+} // fim do programa
