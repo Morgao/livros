7b6d8f81f577eb281cef97fba9cf5275b0bfc2f6
Morgao <contatomorgao@gmail.com>
2024-06-05 04:20:35 -0300

    Create ex_4_13_a.cs

---
+++ CSharpDeitel2003/capitulos/cap4/video_resolucao/ex_de_estudo/ex_4_13_a.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Formats.Asn1;
+using System.IO.Compression;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      // x  9 y 11 depois inverte
+      int x, y;
+      //x = 9;
+      //y = 11;
+       x = 11;
+       y = 9;
+
+      if(x<10)
+      {
+
+      }
+      if(y>10)
+      {
+         Console.WriteLine("*****");
+      }
+      else
+      {
+         Console.WriteLine("#####");
+         Console.WriteLine("$$$$$");
+      }
+
+     
+    } // fim da main
+
+} // fim do programa
