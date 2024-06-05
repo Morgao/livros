520ef1cd9675ffd76b3f2d7adc086bf06bfa2af1
Morgao <contatomorgao@gmail.com>
2024-06-04 23:40:46 -0300

    Create ex_4_1.cs

---
+++ CSharpDeitel2003/capitulos/cap4/video_resolucao/ex_de_estudo/ex_4_1.cs
@@ -0,0 +1,32 @@
+using System;
+namespace teste2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+/*
+a) sequência, seleção, repetição.
+b) if/else.
+c) controlada por contador ou definida.
+d) sentinela, sinal, flag ou fictício.
+e) controle de programa.
+f) Pseudocódigo. 
+g) Palavra-chave.
+h) vazia.
+i) um.
+j) conversão.
+
+
+
+
+*/
+
+
+
+
+        }
+            
+    }
+}
