d2231a4c17cb87779b85f95e9d11e9f9df15b072
Morgao <contatomorgao@gmail.com>
2024-06-04 23:52:24 -0300

    Create ex_4_7_b.cs

---
+++ CSharpDeitel2003/capitulos/cap4/video_resolucao/ex_de_estudo/ex_4_7_b.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Formats.Asn1;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ 
+   
+   int quociente = 5, x = 5;
+
+   quociente /= ++x;
+   Console.WriteLine(quociente);
+      
+
+
+
+
+  
+     
+    } // fim da main
+
+} // fim do programa
