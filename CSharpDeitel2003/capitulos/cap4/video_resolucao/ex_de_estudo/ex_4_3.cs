c04ceb53b41b408b6687bbffd181d3dd62673f1f
Morgao <contatomorgao@gmail.com>
2024-06-04 23:41:22 -0300

    Create ex_4_3.cs

---
+++ CSharpDeitel2003/capitulos/cap4/video_resolucao/ex_de_estudo/ex_4_3.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Diagnostics.Contracts;
+using System.Formats.Asn1;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+ 
+   
+     int c;
+
+     c = 5;
+     Console.WriteLine(c = c + 1);
+     Console.WriteLine(c += 1);
+     Console.WriteLine(c++);
+     Console.WriteLine(++c); 
+     
+      
+
+
+
+
+  
+     
+    } // fim da main
+
+} // fim do programa
